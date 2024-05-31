45956972f2568b857710969c598dc0aaa4886759
srijit0x <163331906+srijit0x@users.noreply.github.com>
2024-05-31 08:10:39 +0200

    cleaning code

--- frontend/CodeEditor.tsx
+++ frontend/CodeEditor.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Editor, { OnMount } from '@monaco-editor/react';
 import { io } from 'socket.io-client';
+
 const SOCKET_IO_SERVER = process.env.REACT_APP_SOCKET_IO_SERVER || 'http://localhost:4000';
 
-function debounce(func, timeout = 300) {
-  let timer;
-  return (...args) => {
+function debounce(func: (...args: any[]) => void, timeout = 300) {
+  let timer: ReturnType<typeof setTimeout>;
+  return (...args: any[]) => {
     clearTimeout(timer);
     timer = setTimeout(() => { func.apply(this, args); }, timeout);
   };
@@ -16,13 +17,11 @@ const CodeEditor: React.FC = () => {
   const editorRef = useRef<any>(null);
   const socketRef = useRef<any>(null);
 
-  // Debounce code update function
   const handleCodeUpdate = debounce((newCode: string) => {
     setCode(newCode);
     socketRef.current.emit('codeChange', { newCode });
   }, 500);
 
-  // Editor configuration and event handling
   const configureEditor = (editor: any) => {
     editor.onDidChangeModelContent(() => {
       const newCode = editor.getValue();
@@ -30,19 +29,17 @@ const CodeEditor: React.FC = () => {
     });
   };
 
-  // Handler to perform actions when the editor is mounted
   const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
     configureEditor(editor);
   };
 
-  // Initialize socket connection and handle incoming code changes
   const initializeSocketConnection = () => {
     socketRef.current = io(SOCKET_IO_SERVER);
     socketRef.current.on('codeChange', (data: { newCode: string }) => {
       const { newCode } = data;
       if (newCode !== code) {
-        editorRef.current.setValue(newCode);
+        editorRef.current?.setValue(newCode);
       }
     });
   };
@@ -50,16 +47,16 @@ const CodeEditor: React.FC = () => {
   useEffect(() => {
     initializeSocketConnection();
     return () => {
-      socketRef.current.disconnect();
+      socketRef.current?.disconnect();
     };
-  }, [code]);
+  }, []);
 
   return (
     <div>
       <Editor
         height="90vh"
         defaultLanguage="javascript"
-        defaultValue="// some comment"
+        defaultValue={code}
         onMount={handleEditorDidMount}
         value={code}
         theme="vs-dark"
